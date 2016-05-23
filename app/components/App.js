b575e92f17255338f2d64a5ecec3636fdb96a163
julianbetancourt <julianbetancourt10@gmail.com>
2016-05-22 19:07:31 -0500

    generate ul>lis from this.state

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import Note from './Note.js';
+import uuid from 'node-uuid';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    };
+  }
+
+
   render() {
-    return <Note />;
+    const notes = this.state.notes;
+    return (
+      <div>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
+      </div>
+    );
   }
 }
