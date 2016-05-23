dd839b9f806743c4e9ceb7e539602128231fcf6d
julianbetancourt <julianbetancourt10@gmail.com>
2016-05-23 16:22:08 -0500

    Communicate Note State changes

--- app/components/App.js
+++ app/components/App.js
@@ -29,7 +29,7 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} />
+        <Notes notes={notes} onEdit={this.editNote}/>
       </div>
     );
   }
@@ -42,6 +42,23 @@ export default class App extends React.Component {
       }])
     });
   };
+
+  editNote = (id, task) => {
+    if(!task.trim()) {
+      return;
+    }
+
+    const notes = this.state.notes.map(note => {
+      if(note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+
+    this.setState({notes});
+  }
+
 
 
 }
