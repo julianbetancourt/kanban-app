aefaa09bc69e907a241425afa8052b29d76eb10a
julianbetancourt <julianbetancourt10@gmail.com>
2016-05-22 20:16:20 -0500

    Add notes component

---
+++ app/components/Notes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Note from './Note.js';
+
+export default ({notes}) => {
+  return (
+    <ul>{notes.map(note =>
+      <li key={note.id}>
+        <Note task={note.task} />
+      </li>
+      )}</ul>
+
+  );
+}
